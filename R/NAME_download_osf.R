11c5cac7153eada48aa806e4e33b6fbaaeffaa36
Sebastian Sauer <ssauer@posteo.de>
2017-06-30 13:14:58 +0200

    bugfixing download_osf

---
+++ R/NAME_download_osf.R
@@ -0,0 +1,21 @@
+#' A function to download data from www.osf.io
+#'
+#' A simple wrapper around download for not having to type a rather cumbersome download link
+#'
+#' This is a convenience function to download data from a specified OSF repo. Internet access is needed.
+#'
+#'
+#' @param repo short link to repo, eg. "meyhp" (chr)
+#' @return data as data.frame
+#'
+#' @examples
+#'
+#'  download_osf("meyhp")
+
+#' @export
+download_osf <- function(repo){
+  repo <- "meyhp"
+  osf_link <- paste0("https://osf.io/",repo, "/?action=download")
+  out <- read.csv(osf_link)
+  base::return(out)
+}
