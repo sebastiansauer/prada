88c5b259b9350f0df054927778cebea54a90158c
Sebastian Sauer <ssauer@posteo.de>
2017-10-31 17:20:32 +0100

    update elec_results

--- R/OBJECT_parties_de.R
+++ R/OBJECT_parties_de.R
@@ -1,6 +1,6 @@
 #' Names of the parties who ran for the German federal elections 2017
 #'
-#' #' This dataset stems from data published by the Bundeswahlleiter 2017
+#' This dataset stems from data published by the Bundeswahlleiter 2017
 #' (c) Der Bundeswahlleiter, Wiesbaden 2017
 #' https://www.bundeswahlleiter.de/info/impressum.html
 #'
@@ -10,7 +10,7 @@
 #' \describe{
 #'   \item{party_short}{The short name (acronym) of the party}
 #'   \item{party_long}{The long (spelled-out) name of the party}
-#'   }
+#' }
 #'
 #'@source This dataset is based on data published by the Bundeswahlleiter 2017, (c) Der Bundeswahlleiter, Wiesbaden 2017, https://www.bundeswahlleiter.de/info/impressum.html
 #'
