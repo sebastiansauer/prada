bca123a9fbc3b39cbf4be7a1e984985bff409cd6
Sebastian Sauer <ssauer@posteo.de>
2022-12-19 21:39:11 +0100

    add get_dag_size()

---
+++ R/NAME_get_dag_size.R
@@ -0,0 +1,18 @@
+#' Get the size of a DAG
+#'
+#' Returns the number of variables in a directed acyclic graph (DAG)
+#'
+#' @param dag, a DAG object from `dagitty`
+#'
+#' @return dag size (integer)
+#' @export
+#'
+#' @importFrom stringr str_extra_all
+#'
+#' @examples
+#'
+#' get_dag_size(my_dag)
+#'
+get_dag_size <- function(dag){
+  stringr::str_extract_all(dag, pattern = "\\d") %>% unlist() %>% max()
+}
