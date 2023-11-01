6dd1571bc698fa3028b8555b54ab218929d2be0b
Sebastian Sauer <ssauer@posteo.de>
2023-11-01 15:03:33 +0100

    bayesbox

--- R/NAME_count_lexicon.R
+++ R/NAME_count_lexicon.R
@@ -15,11 +15,20 @@
 #' 1
 #' @export
 count_lexicon <- function(txt, lexicon){
+  # convert strings to lower letters:
   txt <- tolower(txt)
   lexicon <- tolower(lexicon)
+
+  # build regex query:
   lexicon_regex <- paste0("^", lexicon, "$", collapse = "|")
+
+  # split string into words:
   string_in_words <- unlist(stringr::str_split(txt, pattern = stringr::boundary("word")))
+
+  # search:
   pattern_detected_in_string_count <- sum(stringr::str_detect(string_in_words, pattern = lexicon_regex))
+
+  # return:
   return(pattern_detected_in_string_count)
 }
 
